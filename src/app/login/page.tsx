d9da894ffe807454bb4816d7153fe9854778478d
Jakob Bjelvér <94932025+jakobbjelver@users.noreply.github.com>
2026-02-06 13:25:09 +0100

    Fix log in animation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,16 @@
 import { Footer } from "@/components/Footer";
 import LightRays from "@/components/login/LightRays";
 import LoginContent from "@/components/login/LoginContent";
-import { useMediaQuery } from "@uidotdev/usehooks";
+import { Loader2 } from "lucide-react";
 import { Suspense } from "react";
 
 export default function LoginPage() {
-  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
   return (
     <div className="flex h-svh w-full items-center justify-center p-4 pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)] pl-[env(safe-area-inset-left)] pr-[env(safe-area-inset-right)]">
-      <Suspense fallback={<div className="text-foreground">Loading...</div>}>
+      <Suspense fallback={
+        <div className="text-foreground">
+          <Loader2 className="animate-spin" />
+        </div>}>
         <LoginContent />
         <Footer className="pb-6 absolute bottom-5" />
         <div className="absolute w-screen h-screen top-0 left-0 -z-1">
@@ -17,8 +19,16 @@ export default function LoginPage() {
             followMouse={false}
             raysSpeed={0.5}
             lightSpread={0.9}
-            fadeDistance={isSmallDevice ? 100 : 0.5}
-            />
+            fadeDistance={100}
+            className="block md:hidden"
+          />
+          <LightRays
+            followMouse={false}
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            fadeDistance={0.4}
+            className="md:block hidden"
+          />
         </div>
       </Suspense>
     </div>
